758b839ac647657fdf2d84a2d2ce83be0681af28
aolivas92 <olivas9215@hotmail.com>
2024-12-26 23:06:26 -0700

    Fixed error

--- DICE/DICE_tutorial/DICE_Search.py
+++ DICE/DICE_tutorial/DICE_Search.py
@@ -292,11 +292,8 @@ def dnn_fair_testing(dataset, sens_params, model_path, cluster_num,
     data_config = {"census":census, "credit":credit, "bank":bank, "compas":compas, "default":default,
                   "heart":heart , "diabetes":diabetes,"students":students, "meps15":meps15, "meps16":meps16}
     # prepare the testing data and model
-    if data[dataset] == "census_data2":
-        # The label_encoders can be stored globally instead of this method.
-        X, Y, input_shape, nb_classes, label_encoders = data[dataset]()    
-    else:
-        X, Y, input_shape, nb_classes = data[dataset]()
+    # TODO: Fix later, The label_encoders can be stored globally instead of this method.
+    X, Y, input_shape, nb_classes, label_encoders = data[dataset]()
     tf.set_random_seed(1234)
 
     config = tf.ConfigProto(device_count = {'GPU': 0})
