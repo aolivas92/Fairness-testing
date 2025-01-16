28418c16513b9d9edd760090be801765ca0a278b
aolivas92 <olivas9215@hotmail.com>
2025-01-16 15:18:31 -0700

    Removed the final y value from the categorical_cols

--- DICE/DICE_data/bank.py
+++ DICE/DICE_data/bank.py
@@ -64,8 +64,9 @@ def bank_data2():
     df = pd.DataFrame(raw_data, columns=col_names)
 
     # Find all the categorical cols
+    X_raw = df.iloc[:, :-1]
     categorical_cols = []
-    for indx, value in enumerate(df.iloc[0]):
+    for indx, value in enumerate(X_raw.iloc[0]):
         try:
             int(value)
         except Exception:
@@ -74,7 +75,7 @@ def bank_data2():
     # Get all the values that are possible for categorical columns
     categorical_unique_values = dict()
     for col in categorical_cols:
-        categorical_unique_values[col] = df[col].unique().tolist()
+        categorical_unique_values[col] = X_raw[col].unique().tolist()
 
     # Transform the categorical data in numerical data.
     df_encoded = df.copy()
